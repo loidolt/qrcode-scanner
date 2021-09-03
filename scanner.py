00d7a9cad371b2c5038b36f56d18168f34bc0dae
Chris Loidolt <loidolt@gmail.com>
2021-09-03 17:53:11 -0600

    Debugging

--- scanner.py
+++ scanner.py
@@ -34,14 +34,14 @@ while True:
                 for i in range(len(bbox)):
                     cv2.line(img, tuple(bbox[i][0]), tuple(
                         bbox[(i+1) % len(bbox)][0]), color=(0, 255, 0), thickness=4)
-                    cv2.putText(img, r.content, (int(bbox[0][0][0]), int(
-                        bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.putText(img, r.content, (int(bbox[0][0][0]), int(
+                    bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             else:
                 for i in range(len(bbox)):
                     cv2.line(img, tuple(bbox[i][0]), tuple(
                         bbox[(i+1) % len(bbox)][0]), color=(255, 0, 0), thickness=4)
-                    cv2.putText(img, r.content, (int(bbox[0][0][0]), int(
-                        bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.putText(img, r.content, (int(bbox[0][0][0]), int(
+                    bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     # display the image preview
     cv2.imshow("code detector", img)
     if(cv2.waitKey(1) == ord("q")):
